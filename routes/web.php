bf83680491318b7bcb9bba8a5edf7f8b92cede8e
xuanhoang2702 <ducson.nuce@gmail.com>
2023-07-07 02:24:34 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,10 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 Auth::routes();
-//Language Translation
-Route::get('index/{locale}', [App\Http\Controllers\HomeController::class, 'lang']);
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'root'])->name('root');
 
-//Update User Details
-Route::post('/update-profile/{id}', [App\Http\Controllers\HomeController::class, 'updateProfile'])->name('updateProfile');
-Route::post('/update-password/{id}', [App\Http\Controllers\HomeController::class, 'updatePassword'])->name('updatePassword');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('{any}', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
 require __DIR__ .'/route-layout.php';
 //Upload image in  blog content
 Route::post('/blog/media/upload',[\App\Http\Controllers\Controller::class,'mediaUpload'])->name('media.upload');
