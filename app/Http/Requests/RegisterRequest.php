ddc21d01f00175461b41be234e26d74a3a4688a9
ducson <sontuduc@bizfly.vn>
2023-07-08 11:50:09 +0700

    create auth api

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+            'phone' => ['required', 'min:11'],
+            'name' => 'required'
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'required'=>'không được để trống',
+            'min'=>'tối thiểu :min ký tự'
+        ];
+    }
+    public function attributes()
+    {
+        return [
+            'email' => 'Email người dùng',
+            'password' => 'Mật khẩu'
+        ];
+    }
+}
