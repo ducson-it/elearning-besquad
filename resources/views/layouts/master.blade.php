6ea754ef1d7de2c7b67cc59c96d5b1d4ebc4da9f
xuanhoang2702 <ducson.nuce@gmail.com>
2023-07-17 00:39:02 +0700

    remove mete for firebase

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesbrand" name="author" />
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @vite(['resources/scss/app.scss',
     'resources/js/app.js',
