818a16cbc1ffad18ddb292fbeb6780ce6b78f327
thanh nam <thanhdhph18884@fpt.edu.vn>
2023-07-19 00:00:04 +0700

    search các trang và fix các bug , viết api;

---
+++ app/Http/Requests/ChangeUserRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ChangeUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'avatar' => 'nullable|string',
+            'phone' => 'nullable|string',
+            'address' => 'nullable|string',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Vui lòng nhập tên.',
+            'name.string' => 'Tên phải là một chuỗi.',
+            'email.required' => 'Vui lòng nhập địa chỉ email.',
+            'email.email' => 'Địa chỉ email không hợp lệ.',
+            'avatar.required' => 'Avatar không được trống',
+            'phone.string' => 'Số điện thoại phải là một chuỗi.',
+            'address.required' => 'Địa chỉ không được trống',
+        ];
+    }
+}
