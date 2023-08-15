820c576f1f3722fe106cb941cdd3643350f5a4f5
thanh nam <thanhdhph18884@fpt.edu.vn>
2023-08-16 01:39:39 +0700

    thanhcode

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,8 +8,8 @@ use App\Models\Lesson;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Comment extends Model
 {
